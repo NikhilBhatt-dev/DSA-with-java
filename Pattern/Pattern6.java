4b94a5b9f3293279863604091449eae907e8db38
Nikhil Bhatt <bhattnikhil158@gmail.com>
2025-11-10 18:51:19 +0530

    adding file

---
+++ Pattern/Pattern6.java
@@ -0,0 +1,25 @@
+public class Pattern6 {
+    public static void main(String[] args) {
+        pattern28(5);
+    }
+
+    static void pattern28(int n) {
+        for (int row = 1; row <= 2 * n - 1; row++) {
+            int totalColsInRow = row > n ? 2 * n - row : row;
+            int noOfSpaces = n - totalColsInRow;
+
+            // Print spaces
+            for (int s = 0; s < noOfSpaces; s++) {
+                System.out.print(" ");
+            }
+
+            // Print stars
+            for (int col = 0; col < totalColsInRow; col++) {
+                System.out.print("* ");
+            }
+
+            // New line
+            System.out.println();
+        }
+    }
+}
