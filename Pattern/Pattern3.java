c4cf8f17ee370eacf2c70e08a6b63e49ed5f7306
Nikhil Bhatt <bhattnikhil158@gmail.com>
2025-11-10 18:11:50 +0530

    slove pattern3

---
+++ Pattern/Pattern3.java
@@ -0,0 +1,20 @@
+public class Pattern3 {
+    public static void main(String[] args) {
+        pattern3(4);
+        
+    }
+
+    static void pattern3(int n){ 
+        for (int row = 1; row <= n; row++) {
+             // for every row , run the column
+            for( int col = 1; col <= n-row+1; col++){
+                System.out.print("* ");
+                 
+            }  
+            // one row is printed we ned to add a newline          
+        
+
+        System.out.println();
+        } 
+    }
+}
