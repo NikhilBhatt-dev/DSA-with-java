3a0981c3b26a442df6168ffd4c42cdffc6dd1c4e
Nikhil Bhatt <bhattnikhil158@gmail.com>
2025-11-11 18:20:15 +0530

    commit

--- Recursion/Main.java
+++ Recursion/Main.java
@@ -1,5 +1,16 @@
-package Recursion;
-
 public class Main {
+    public static void main(String[] args) {
+        message(5);
+        // write a function hello world
+    }
     
-}
+    static void message( int n) {
+        for (int i = 0; i <= 5; i++) {
+             System.out.println("hello world");
+        }
+
+       
+    }
+
+    }
+
