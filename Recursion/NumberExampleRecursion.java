c035f13b8061f9c5a4760b7db27c39962811477b
Nikhil Bhatt <bhattnikhil158@gmail.com>
2025-11-11 19:25:21 +0530

    update the file

--- Recursion/NumberExampleRecursion.java
+++ Recursion/NumberExampleRecursion.java
@@ -1,15 +1,16 @@
-// package Recursion;
-
+package Recursion;
 public class NumberExampleRecursion {
     public static void main(String[] args) {
         print(1);
-        
     }
 
-    static void print( int n){
-        System.out.println(n);
-        print(n+ 1);
+    static void print(int n) {
+        // base condition
+        if (n > 5) {
+            return; // stop recursion
+        }
+
+        System.out.println(n); // print current number
+        print(n + 1);          // recursive call
     }
-    
 }
-
