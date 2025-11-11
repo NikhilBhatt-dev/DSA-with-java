9269135cc77c759b3c35bbcd98ffc6eac76b7da4
Nikhil Bhatt <bhattnikhil158@gmail.com>
2025-11-11 19:08:57 +0530

    run this file

---
+++ Recursion/NumberExampleRecursion.java
@@ -0,0 +1,15 @@
+// package Recursion;
+
+public class NumberExampleRecursion {
+    public static void main(String[] args) {
+        print(1);
+        
+    }
+
+    static void print( int n){
+        System.out.println(n);
+        print(n+ 1);
+    }
+    
+}
+
