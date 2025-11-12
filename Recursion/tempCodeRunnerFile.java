40d4f9e3be746fdad0c6d4b4b90bf6953f8e8dc0
Nikhil Bhatt <bhattnikhil158@gmail.com>
2025-11-12 18:56:14 +0530

    fibonachi

--- Recursion/tempCodeRunnerFile.java
+++ Recursion/tempCodeRunnerFile.java
@@ -1,1 +1,14 @@
-package Recursion;
+public class Fibonacci {
+ public static void main(String[] args) {
+    fibo(5)
+    
+ }  
+ 
+ static int fibo (int n) {
+    if(n <2){
+        return n;
+
+    }
+    return fibo(n-1) + fibo(n-2);
+ }
+}
