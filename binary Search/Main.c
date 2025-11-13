1e3726b4df14c008270530bb2bbe64f0e81802f9
Nikhil Bhatt <bhattnikhil158@gmail.com>
2025-11-13 12:33:15 +0530

    inital commit

---
+++ binary Search/Main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, j;
+
+    printf("Enter number of activities: ");
+    scanf("%d", &n);
+
+    int start[n], finish[n];
+
+    printf("Enter start times:\n");
+    for(i = 0; i < n; i++)
+        scanf("%d", &start[i]);
+
+    printf("Enter finish times (sorted in ascending order):\n");
+    for(i = 0; i < n; i++)
+        scanf("%d", &finish[i]);
+
+    printf("Selected activities are: ");
+
+    
+    i = 0;
+    printf("%d ", i + 1);
+
+    // Check for the rest of the activities
+    for(j = 1; j < n; j++) {
+        if(start[j] >= finish[i]) {
+            printf("%d ", j + 1);
+            i = j;
+        }
+    }
+
+    return 0;
+}
