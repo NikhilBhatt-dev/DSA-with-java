c55ee2059b7ffb50c32129fb42a7650f95d691d4
Nikhil Bhatt <bhattnikhil158@gmail.com>
2026-03-16 19:30:02 +0530

    update comment

--- Recursion/Merge.java
+++ Recursion/Merge.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 public class Merge {
     public static void main(String[] args) {
 
-        int[] arr ={10,2,23,134,35,11,23,45,6,7,9};
+        int[] arr ={1,3,5,2,4,6};
 
         
         arr= merge(arr);
@@ -18,7 +18,7 @@ public class Merge {
 
         int mid  = arr.length / 2;
 
-        int[] left =  merge(Arrays.copyOfRange(arr, 0, mid));
+        int[] left =  merge(Arrays.copyOfRange(arr, 0, mid)); // (int[] original, int from, int to)
         int[] right =  merge(Arrays.copyOfRange(arr, mid, arr.length));
          
         return merge(left, right);
@@ -32,7 +32,7 @@ public class Merge {
         int k = 0;
 
         while (i < first.length && j < second.length) {
-            if (first[i] < second[j]) {
+            if (first[i] <= second[j]) {
                 mix[k] = first[i];
                 i++;
             } else {
