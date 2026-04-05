3c22d08d462df8a4c957e6f6b12117216888d9ea
Nikhil Bhatt <bhattnikhil158@gmail.com>
2026-04-05 22:19:47 +0530

    main.jav

---
+++ Oops/staticExample/Human.java
@@ -0,0 +1,17 @@
+package staticExample;
+
+public class Human {
+    int age;
+    String name;
+    int salary;
+    boolean marriedOrNot; 
+
+    public Human(int age, String name, int salary, boolean marriedOrNot) {
+        this.age = age;
+        this.name = name;
+        this.salary = salary;
+        this.marriedOrNot = marriedOrNot;
+    }
+    
+    
+}
---
+++ Oops/staticExample/Main.java
@@ -0,0 +1,10 @@
+package staticExample;
+
+public class Main {
+    public static void main(String[] args) {
+        Human nikhil = new Human(20,"Nikhil",200000,false);
+        Human manoj = new Human(20,"Manoj",200001,false);
+
+        System.out.println(nikhil.age);
+    }
+}
