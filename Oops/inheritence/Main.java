268bdebdbb30678de7f6104057194046b723dbc5
Nikhil Bhatt <bhattnikhil158@gmail.com>
2026-04-06 22:44:54 +0530

    packages

--- Oops/inheritence/Main.java
+++ Oops/inheritence/Main.java
@@ -3,7 +3,7 @@ package inheritence;
 
 public class Main {
     public static void main(String[] args) {
-        Box box = new Box(5);
+        Box box = new Box(5) ;
         box.information();
         System.out.println(box.l+" "+box.w+" "+box.h);
     }
