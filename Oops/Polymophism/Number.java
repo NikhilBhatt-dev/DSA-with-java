82eb22e68c463cd9895dc606714abe1ad1050b0e
Nikhil Bhatt <bhattnikhil158@gmail.com>
2026-04-09 10:46:37 +0530

    Method overloading

---
+++ Oops/Polymophism/Number.java
@@ -0,0 +1,20 @@
+package Polymophism;
+
+//Method Overloading
+
+
+public class Number {
+    int sum(int a, int b){
+        return a+b;
+
+    }    
+    int sum(int a, int b, int c){
+        return a+b+c;
+
+    }
+    public static void main(String[] args) {
+        Number number = new Number();
+        number.sum(2, 3);
+        number.sum(2, 3, 4);
+    }
+}
