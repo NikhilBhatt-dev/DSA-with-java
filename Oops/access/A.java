97d52232d1949f82b89f4017c0117d06e6ca5016
Nikhil Bhatt <bhattnikhil158@gmail.com>
2026-04-10 22:35:22 +0530

    access modifier

---
+++ Oops/access/A.java
@@ -0,0 +1,20 @@
+package access;
+
+public class A {
+
+    int num;
+
+    public A(int num) {
+        this.num = num;
+        String name;
+        int[] arr;
+
+    }
+
+    public A(int num, String name, int[] arr) {
+        this.num = num;
+        this.name = name;
+        this.arr = new int[num];
+    }
+
+}
---
+++ Oops/access/Main.java
@@ -0,0 +1,11 @@
+package access;
+
+public class Main {
+    public static void main(String[] args) {
+        A obj  = new A(10,"nikhil");
+        obj.arr();
+        //acces the data member
+        //modify the data member
+    }
+    
+}
