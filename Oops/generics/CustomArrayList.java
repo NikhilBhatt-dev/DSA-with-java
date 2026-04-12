da8092eb83c2fd43a659b898e37dc77e1a32a83b
Nikhil Bhatt <bhattnikhil158@gmail.com>
2026-04-12 15:55:28 +0530

    custom arraylist

--- Oops/generics/CustomArrayList.java
+++ Oops/generics/CustomArrayList.java
@@ -3,14 +3,42 @@ package generics;
 import java.util.ArrayList;
 
 public class CustomArrayList{
+
+    private int[] data;
+
+    private static int DEFAULT_SIZE = 10;
+    private int size = 0;
+
+    public CustomArrayList() {
+        this.data = new int[DEFAULT_SIZE];
+    }
+
+    public void add(int num) {
+        if (isFull()) {
+            resize();
+        }
+        data[size++] = num;
+        
+    }
+
+    private boolean isFull() {
+        return size == data.length;
+    }
+
+    private void resize() {
+        int[] temp = new int[data.length * 2];
+
+        //copy the current item in the new array
+        
+        for (int i = 0; i < data.length; i++) {
+            temp[i] = data[i];
+        }
+        data = temp;
+    }
+
     public static void main(String[] args) {
         ArrayList list = new ArrayList();
-        list.add(1);
-        list.add("Hello");
-        list.remove(0);
-        list.get(0);
-        list.set(1, 2233);
-        list.size();
-        list.isEmpty();
+        
+      
     }
 }
