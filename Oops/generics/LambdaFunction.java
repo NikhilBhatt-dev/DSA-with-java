7f795d19f3287d9822d462cc50b2b87a4c9f406f
Nikhil Bhatt <bhattnikhil158@gmail.com>
2026-04-13 16:56:45 +0530

    lambda fn

---
+++ Oops/generics/LambdaFunction.java
@@ -0,0 +1,9 @@
+package generics;
+import java.util.function.Function;
+
+public class LambdaFunction {
+    public static void main(String[] args) {
+        Function<Integer, Integer> square = x -> x * x;
+        System.out.println(square.apply(5));
+    }
+}
