1db5de5447d2afc40b149121b842ddc4f91f0c3d
Nikhil Bhatt <bhattnikhil158@gmail.com>
2026-04-14 18:28:53 +0530

    vector

--- Oops/collection/Main.java
+++ Oops/collection/Main.java
@@ -8,6 +8,14 @@ public class Main{
     public static void main(String[] args) {
         List<Integer> list = new ArrayList<>();
         List<Integer> list2 = new LinkedList<>();
+
+
+        list2.add(22);
+        list.add(23);
+
+        list.get(0);
+        System.out.println(list2);
+        list2.isEmpty();
         
     }
 }
