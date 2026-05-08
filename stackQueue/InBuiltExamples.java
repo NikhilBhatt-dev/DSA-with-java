c364d842c7d73599efc0685b1c347138798a98a9
Nikhil Bhatt <bhattnikhil158@gmail.com>
2026-05-08 21:37:27 +0530

    queue

--- stackQueue/InBuiltExamples.java
+++ stackQueue/InBuiltExamples.java
@@ -1,26 +1,42 @@
 // package stackQueue;
 
-import java.util.Stack;
+
+//stack is classs but queue is a interface
+
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class InBuiltExamples {
     public static void main(String[] args) {
-        Stack<Integer> stack = new Stack<>();
-        stack.push(34);
-        stack.push(20);
-        stack.push(24);
-        stack.push(30);
-        stack.push(44);
-        stack.push(50);
+        // Stack<Integer> stack = new Stack<>();
+        // stack.push(34);
+        // stack.push(20);
+        // stack.push(24);
+        // stack.push(30);
+        // stack.push(44);
+        // stack.push(50);
 
-        System.out.println(stack.pop());
+        // System.out.println(stack.pop());
         
-        System.out.println(stack.pop());
+        // System.out.println(stack.pop());
         
-        System.out.println(stack.pop());
+        // System.out.println(stack.pop());
         
-        System.out.println(stack.pop());
+        // System.out.println(stack.pop());
 
-        System.out.println(stack.pop());
+        // System.out.println(stack.pop());
+
+
+        Queue<Integer>  queue = new  LinkedList<>();
+        queue.add(3);
+        queue.add(32);
+
+        queue.add(33);
+        queue.add(34);
+
+        queue.add(35);
+
+        System.out.println(queue.peek());
         
 
     }
